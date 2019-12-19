bc28db1ee686e763e0c307633277ffb3e0e8aa90
itxing <itxing666@163.com>
2019-12-19 16:49:40 +0800

    修改

--- 07-http-url.js
+++ 07-http-url.js
@@ -3,6 +3,7 @@ let server = http.createServer()
 server.on('request', function(req, res) {
   console.log('============',req.url)
   // 根据不同的响应路径获取获取不同的结果 /开头的
+  console.log('=======请求我的端口号', req.socket.remoteAddress, req.socket.remotePort)
   const url = req.url
   if (url === '/') {
     res.end('index page')
