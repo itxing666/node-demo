4df0b0fc73c0517db5caee9c2b826ab7a27d6b76
itxing <itxing666@163.com>
2019-12-19 15:56:10 +0800

    加载与导出

---
+++ 加载与导出/a.js
@@ -0,0 +1,3 @@
+let b = require('./b.js')
+console.log('==============', b.foo)
+console.log('==============add', b.add(1, 2))
---
+++ 加载与导出/b.js
@@ -0,0 +1,4 @@
+exports.foo = 'hello'
+exports.add = function add(x, y) {
+  return x + y
+}
